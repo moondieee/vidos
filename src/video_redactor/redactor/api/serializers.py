e4022e062d3332e399bb8900bff3fe68cb888d5a
eslupmi101 <ajsen-andreev@yandex.ru>
2024-01-14 02:16:25 +0900

    Add a complete video editor without tests, utilizing DRF, DRF-Mongoengine, and MongoDB.

---
+++ src/video_redactor/redactor/api/serializers.py
@@ -0,0 +1,119 @@
+from rest_framework_mongoengine import serializers as mongoserializers
+from rest_framework import serializers
+
+from core.shortcuts import get_object_or_404
+from ..models import Button, Video, VideoWidget
+from ..utils import get_video_from_widget
+
+
+class ButtonSerializer(mongoserializers.EmbeddedDocumentSerializer):
+    created = serializers.DateTimeField(
+        source='id.generation_time',
+        read_only=True
+    )
+
+    class Meta:
+        model = Button
+        fields = (
+            'id',
+            'type',
+            'name',
+            'video_id',
+            'created',
+        )
+        read_only_fields = (
+            'id',
+            'created',
+        )
+
+    def create(self, validated_data):
+        request = self.context['request']
+        kwargs = self.context['view'].kwargs
+
+        widget = get_object_or_404(
+            VideoWidget,
+            user_id=request.user.id,
+            id=kwargs.get('widget_id')
+        )
+        video = get_video_from_widget(
+            widget,
+            kwargs.get('video_id')
+        )
+        button = super().create(validated_data)
+        # MongoDB EmbeddedDocument add by append method
+        video.buttons.append(button)
+
+        # Saving all Mongo Document with EmbeddedDocuments
+        widget.save()
+        return button
+
+
+class VideoSerializer(mongoserializers.EmbeddedDocumentSerializer):
+    created = serializers.DateTimeField(
+        source='id.generation_time',
+        read_only=True
+    )
+    buttons = ButtonSerializer(
+        many=True,
+        required=False,
+        read_only=True
+    )
+
+    class Meta:
+        model = Video
+        fields = (
+            'id',
+            'name',
+            'video_url',
+            'buttons',
+            'created',
+        )
+        read_only_fields = (
+            'id',
+            'video_url',
+            'buttons',
+            'created',
+        )
+
+    def create(self, validated_data):
+        request = self.context['request']
+        kwargs = self.context['view'].kwargs
+
+        # Get the widget that should have this created video
+        widget = get_object_or_404(
+            VideoWidget,
+            user_id=request.user.id,
+            id=kwargs.get('widget_id')
+        )
+        video = super().create(validated_data)
+        # MongoDB EmbeddedDocument add by append method
+        widget.videos.append(video)
+        widget.save()
+        return video
+
+
+class VideoWidgetSerializer(mongoserializers.DocumentSerializer):
+    videos = VideoSerializer(
+        many=True,
+        read_only=True
+    )
+    created = serializers.DateTimeField(
+        source='id.generation_time',
+        read_only=True
+    )
+
+    class Meta:
+        model = VideoWidget
+        fields = (
+            'id',
+            'website_id',
+            'user_id',
+            'videos',
+            'created',
+        )
+        read_only_fields = (
+            'id',
+            'user_id',
+            'videos',
+            'created',
+        )
